7a0cc8c98a8a6f00b333c4571d5df3e124b245e7
behzad <behzadfareed@gmail.com>
2019-10-31 22:45:16 +0330

    sss

--- api/api.py
+++ api/api.py
@@ -4,48 +4,14 @@ api.py
   REST requests and responses
 """
 
-from functools import wraps
-from datetime import datetime, timedelta
-
 from flask import Blueprint, jsonify, request, current_app
 
-import jwt
-
 from .models import db, Item, User
+from flask_jwt_extended import (
+    jwt_required, create_access_token,
+    get_jwt_identity)
 
 api = Blueprint('api', __name__)
-
-def token_required(f):
-    @wraps(f)
-    def _verify(*args, **kwargs):
-        auth_headers = request.headers.get('Authorization', ' ').split()
-        invalid_msg = {
-            'message': 'Invalid token. Registeration and / or authentication required',
-            'authenticated': False
-        }
-        expired_msg = {
-            'message': 'Expired token. Reauthentication required.',
-            'authenticated': False
-        }
-
-        if len(auth_headers) != 2:
-            return jsonify(invalid_msg), 401
-
-        try:
-            token = auth_headers[1]
-            data = jwt.decode(token, current_app.config['SECRET_KEY'])
-            user = User.query.filter_by(email=data['sub']).first()
-            print(data)
-            if not user:
-                raise RuntimeError('User not found')
-            return f(*args, **kwargs)
-        except jwt.ExpiredSignatureError:
-            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
-        except (jwt.InvalidTokenError, Exception) as e:
-            print(e)
-            return jsonify(invalid_msg), 401
-
-    return _verify
 
 @api.route('/')
 def home():
@@ -68,34 +34,30 @@ def login():
     if not user:
         return jsonify({ 'message': 'Invalid credentials', 'authenticated': False }), 401
 
-    token = jwt.encode({
-        'sub': user.email,
-        'iat':datetime.utcnow(),
-        'exp': datetime.utcnow() + timedelta(minutes=30)},
-        current_app.config['SECRET_KEY'])
-    return jsonify({ 'token': token.decode('UTF-8') })
+    token = access_token = create_access_token(identity=user.id)
+    return jsonify({ 'token': token })
 
 
 @api.route('/items', methods=('POST',))
-@token_required
-def create_item(current_user):
+@jwt_required
+def create_item():
     data = request.get_json()
     item = Item(name=data['name'], details=data['details'])
-    item.creator = current_user
+    item.creator = User.query.get(get_jwt_identity())
     db.session.add(item)
     db.session.commit()
     return jsonify(item.to_dict()), 201
 
 
 @api.route('/items', methods=('GET',))
-@token_required
+@jwt_required
 def fetch_items():
-    items = Item.query.all()
+    items = Item.query.filter_by(creator_id=get_jwt_identity())
     return jsonify([i.to_dict() for i in items])
 
 
 @api.route('/items/<int:id>', methods=('GET', 'PUT'))
-@token_required
+@jwt_required
 def item(id):
     if request.method == 'GET':
         item = Item.query.get(id)
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object('api.config.BaseConfig')
+jwt = JWTManager(app)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
